49ef6798f474d6d9ee5db4e6c155cd27a889cfc4
Anastasia Kutsevolova <anastasiasemenok@gmail.com>
2022-07-15 17:59:27 -0600

    added closing and destroy options

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const modal = $.modal();
+const modal = $.modal({
+  title: "Anastasia Modal",
+  closable: true,
+  content: `
+        <p>Lorem ipsum dolor set. </p>
+        <p>Lorem ipsum dolor set. </p>`,
+
+  width: "400px",
+  footerButtons: [
+    {
+      text: "Ok",
+      type: "primary",
+      handler() {
+        console.log("Primary btn clicked");
+      },
+    },
+    {
+      text: "Cancel",
+      type: "danger",
+      handler() {
+        console.log("Danger btn clicked");
+      },
+    },
+  ],
+});
--- plug/modal.js
+++ plug/modal.js
@@ -1,21 +1,31 @@
+function _createModalFooter(buttons = []) {
+  if (buttons.length === 0) {
+    return document.createElement("div");
+  }
+  const wrap = document.createElement("div");
+  wrap.classList.add("modal-footer");
+
+  return wrap;
+}
+
 function _createModal(options) {
+  const DEFAULT_WIDTH = "600px";
   const modal = document.createElement("div");
   modal.classList.add("vmodal");
   modal.insertAdjacentHTML(
     "afterbegin",
-    `<div class="modal-overlay">
-        <div class="modal-window">
+    `<div class="modal-overlay" data-close="true">
+        <div class="modal-window" style="width: ${options.width}">
           <div class="modal-header">
-            <span class="modal-title">Modal Title</span>
-            <span class="modal-close">&times;</span>
+            <span class="modal-title">${options.title || "Window"}</span>
+            ${
+              options.closable
+                ? `<span class="modal-close" data-close="true">&times;</span> `
+                : ""
+            }
           </div>
-          <div class="modal-body">
-            <p>Lorem ipsum dolor sit.</p>
-            <p>Lorem ipsum dolor sit.</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-dark">Ok</button>
-            <button type="button" class="btn btn-outline-dark">Cancel</button>
+          <div class="modal-body" data-content>
+            ${options.content || ""}
           </div>
         </div>
     </div>`
@@ -28,9 +38,13 @@ $.modal = function (options) {
   const ANIMATION_SPEED = 200;
   const $modal = _createModal(options);
   let closing = false;
+  let desrtoyed = false;
 
-  return {
+  const modal = {
     open() {
+      if (desrtoyed) {
+        return console.log("Modal is destroyed");
+      }
       !closing && $modal.classList.add("open");
     },
     close() {
@@ -42,6 +56,24 @@ $.modal = function (options) {
         closing = false;
       }, ANIMATION_SPEED);
     },
-    destroy() {},
   };
+
+  const listener = (event) => {
+    if (event.target.dataset.close) {
+      modal.close();
+    }
+  };
+
+  $modal.addEventListener("click", listener);
+
+  return Object.assign(modal, {
+    desrtoy() {
+      $modal.parentNode.removeChild($modal);
+      $modal.removeEventListener("click", listener);
+      desrtoyed = true;
+    },
+    setContent(html) {
+      $modal.querySelector("[data-content]").innerHTML = html;
+    },
+  });
 };
